a88a2cb37f178830c585fc10998a6e94488e1ba5
saadwika <saadwikadaparthi135@gmail.com>
2024-09-07 20:58:58 +0530

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,4 @@
+print("hello world")
+print (
+    'whats up with day 1??'
+)
---
+++ sum.py
@@ -0,0 +1,6 @@
+num1=14
+num2=100
+sum=num1+num2
+diff=num1-num2
+print("sum of",num1,"and",num2,"is..",sum)
+print("diff of",num1,"and",num2,"is..",diff)
